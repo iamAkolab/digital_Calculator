36c7c594be229852a1e2ee0cb9f61c67ffcf2fbb
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-03-13 23:59:36 +0100

    create widget func...

--- calculator.py
+++ calculator.py
@@ -8,3 +8,16 @@ class Application(Frame):
         self.UserIn = StringVar()
         self.grid()
         self.create_widgets()
+
+    def create_widgets(self):
+        self.user_input = Entry(self, bg = "#5BC8AC", 
+        bd = 29, insertwidth = 4, width = 24,
+        font = ("Verdana", 20, "bold"), textvariable = self.UserIn, justify = RIGHT)
+        self.user_input.grid(columnspan = 4)
+
+        self.user_input.insert(0,"0")
+
+        self.button1 = Button(self, bg = "#95dbc6", bd= 12,
+        text = "7", padx = 33, pady = 25,
+        font= ("Helvetica", 20, "bold"), command = lambda: self.buttonClick(7)
+        self.button1.grid(row = 2, column = 0 , sticky = W)
