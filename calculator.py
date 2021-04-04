f541390d951dff72c4dc13e9edfd66ca0f59e542
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-04-05 00:09:02 +0100

    added buttons

--- calculator.py
+++ calculator.py
@@ -17,7 +17,82 @@ class Application(Frame):
 
         self.user_input.insert(0,"0")
 
-        self.button1 = Button(self, bg = "#95dbc6", bd= 12,
+        self.button7 = Button(self, bg = "#95dbc6", bd= 12,
         text = "7", padx = 33, pady = 25,
-        font= ("Helvetica", 20, "bold"), command = lambda: self.buttonClick(7)
-        self.button1.grid(row = 2, column = 0 , sticky = W)
+        font= ("Helvetica", 20, "bold"), command = lambda: self.buttonClick(7))
+        self.button7.grid(row = 2, column = 0 , sticky = W)
+
+        self.button8 = Button(self, bg = "#95dbc6", bd= 12,
+        text = "8", padx = 33, pady = 25,
+        font= ("Helvetica", 20, "bold"), command = lambda: self.buttonClick(8))
+        self.button8.grid(row = 2, column = 1 , sticky = W)
+
+        self.button9 = Button(self, bg = "#95dbc6", bd= 12,
+        text = "9", padx = 33, pady = 25,
+        font= ("Helvetica", 20, "bold"), command = lambda: self.buttonClick(9))
+        self.button9.grid(row = 2, column = 2 , sticky = W)
+
+        self.button4 = Button(self, bg = "#95dbc6", bd= 12,
+        text = "4", padx = 33, pady = 25,
+        font= ("Helvetica", 20, "bold"), command = lambda: self.buttonClick(4))
+        self.button4.grid(row = 3, column = 0 , sticky = W)
+
+        self.button5 = Button(self, bg = "#95dbc6", bd= 12,
+        text = "5", padx = 33, pady = 25,
+        font= ("Helvetica", 20, "bold"), command = lambda: self.buttonClick(5))
+        self.button5.grid(row = 3, column = 1 , sticky = W)
+
+        self.button5 = Button(self, bg = "#95dbc6", bd= 12,
+        text = "6", padx = 33, pady = 25,
+        font= ("Helvetica", 20, "bold"), command = lambda: self.buttonClick(6))
+        self.button5.grid(row = 3, column = 2 , sticky = W)
+
+        self.button1 = Button(self, bg = "#95dbc6", bd= 12,
+        text = "1", padx = 33, pady = 25,
+        font= ("Helvetica", 20, "bold"), command = lambda: self.buttonClick(1))
+        self.button1.grid(row = 4, column = 0 , sticky = W)
+
+        self.button2 = Button(self, bg = "#95dbc6", bd= 12,
+        text = "2", padx = 33, pady = 25,
+        font= ("Helvetica", 20, "bold"), command = lambda: self.buttonClick(2))
+        self.button2.grid(row = 4, column = 1 , sticky = W)
+
+        self.button3 = Button(self, bg = "#95dbc6", bd= 12,
+        text = "3", padx = 33, pady = 25,
+        font= ("Helvetica", 20, "bold"), command = lambda: self.buttonClick(3))
+        self.button3.grid(row = 4, column = 2 , sticky = W)
+
+        self.button0 = Button(self, bg = "#95dbc6", bd= 12,
+        text = "0", padx = 33, pady = 25,
+        font= ("Helvetica", 20, "bold"), command = lambda: self.buttonClick(3))
+        self.button3.grid(row = 5, column = 0 , sticky = W)
+
+        self.AddButton = Button(self, bg = "#95dbc6", bd= 12,
+        text = "+", padx = 33, pady = 25,
+        font= ("Helvetica", 20, "bold"), command = lambda: self.buttonClick("+"))
+        self.AddButton.grid(row = 2, column = 3 , sticky = W)
+
+        self.SubButton = Button(self, bg = "#95dbc6", bd= 12,
+        text = "-", padx = 33, pady = 25,
+        font= ("Helvetica", 20, "bold"), command = lambda: self.buttonClick("-"))
+        self.SubButton.grid(row = 3, column = 3 , sticky = W)
+
+        self.MultButton = Button(self, bg = "#95dbc6", bd= 12,
+        text = "*", padx = 33, pady = 25,
+        font= ("Helvetica", 20, "bold"), command = lambda: self.buttonClick("*"))
+        self.MultButton.grid(row = 4, column = 3 , sticky = W)
+
+        self.DivButton = Button(self, bg = "#95dbc6", bd= 12,
+        text = "/", padx = 33, pady = 25,
+        font= ("Helvetica", 20, "bold"), command = lambda: self.buttonClick("/"))
+        self.DivButton.grid(row = 5, column = 3 , sticky = W)
+
+        self.EqualButton = Button(self, bg = "#E6D72A", bd= 12,
+        text = "=", padx = 33, pady = 25,
+        font= ("Helvetica", 20, "bold"), command = lambda: self.buttonClick("="))
+        self.EqualButton.grid(row = 5, column = 1 , sticky = W)
+
+        self.ClearButton = Button(self, bg = "#E6D72A", bd= 12,
+        text = "AC", width = 28, padx = 7,
+        font= ("Helvetica", 20, "bold"), command = self.ClearDisplay)
+        self.ClearButton.grid(row = 1, column = 4 , sticky = W)
